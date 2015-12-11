a19c88926cbfceec7f8ab9b27bb6b460ad336006
hua <mzhua78@hotmail.com>
2015-12-11 22:55:03 +0800

    add bottom bar to main

--- app/src/main/java/com/wonders/xlab/pci/MainActivity.java
+++ app/src/main/java/com/wonders/xlab/pci/MainActivity.java
@@ -1,20 +1,40 @@
 package com.wonders.xlab.pci;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.wonders.xlab.common.viewpager.ScrollableViewPager;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+    @Bind(R.id.vp_main)
+    ScrollableViewPager mVpMain;
+    @Bind(R.id.fab_main)
+    FloatingActionButton mFab;
+    @Bind(R.id.tl_main)
+    TabLayout mTabLayout;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mTabLayout.addTab(mTabLayout.newTab().setText("首页"));
+        mTabLayout.addTab(mTabLayout.newTab().setText("档案"));
     }
 
     @Override
