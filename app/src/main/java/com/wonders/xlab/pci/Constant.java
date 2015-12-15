3c5c50bb26acf76779f26f648c8fdd3a0c9583cc
hua <mzhua78@hotmail.com>
2015-12-15 14:49:33 +0800

    update

--- app/src/main/java/com/wonders/xlab/pci/Constant.java
+++ app/src/main/java/com/wonders/xlab/pci/Constant.java
@@ -4,6 +4,6 @@ package com.wonders.xlab.pci;
  * Created by hua on 15/12/13.
  */
 public class Constant {
-//    public static String BASE_FEED = "http://101.231.124.8:45698/fullway-health/";
-    public static String BASE_FEED = "http://172.16.76.185:8080/";
+//    public static String BASE_FEED = "http://172.16.76.185:8080/";
+    public static String BASE_FEED = "http://172.16.74.6:8080/pci-user/";
 }
