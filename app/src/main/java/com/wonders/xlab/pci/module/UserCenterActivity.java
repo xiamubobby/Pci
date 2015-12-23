d588211be84cfd2f811b2e243528f11b1ea21b5a
hua <mzhua78@hotmail.com>
2015-12-23 10:29:30 +0800

    update

--- app/src/main/java/com/wonders/xlab/pci/module/UserCenterActivity.java
+++ app/src/main/java/com/wonders/xlab/pci/module/UserCenterActivity.java
@@ -1,13 +1,25 @@
 package com.wonders.xlab.pci.module;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.wonders.xlab.pci.R;
+import com.wonders.xlab.pci.application.AIManager;
+import com.wonders.xlab.pci.application.SPManager;
 import com.wonders.xlab.pci.module.base.AppbarActivity;
+import com.wonders.xlab.pci.module.login.LoginActivity;
 
+import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
+import me.drakeet.labelview.LabelView;
 
 public class UserCenterActivity extends AppbarActivity {
+
+    @Bind(R.id.tv_user_center_tel)
+    LabelView mTvUserCenterTel;
+    @Bind(R.id.tv_user_center_medicare_card)
+    LabelView mTvUserCenterMedi;
 
     @Override
     public int getContentLayout() {
@@ -23,6 +35,14 @@ public class UserCenterActivity extends AppbarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ButterKnife.bind(this);
+        mTvUserCenterTel.setText(AIManager.getInstance(this).getUserTel());
+        mTvUserCenterMedi.setText(AIManager.getInstance(this).getMedicareCard());
     }
 
+    @OnClick(R.id.btn_user_center_exit)
+    public void exit() {
+        SPManager.get(this).clear();
+        startActivity(new Intent(this, LoginActivity.class));
+        finish();
+    }
 }
