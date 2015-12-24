41e6f2a6943f946e3cbad3f7954754bb3b641486
hua <mzhua78@hotmail.com>
2015-12-24 10:38:17 +0800

    fix a emchat bug

--- app/src/main/java/com/wonders/xlab/pci/Constant.java
+++ app/src/main/java/com/wonders/xlab/pci/Constant.java
@@ -7,7 +7,8 @@ public class Constant {
     public final static String TEST_PORTRAIT = "http://i0.wp.com/www.gottabemobile.com/wp-content/uploads/2015/08/Android-6.0-official.png";
 
     //    public final static String BASE_FEED = "http://172.16.76.185:8080/";
-    public static String BASE_FEED = "http://172.16.74.6:8080/pci-user/";
+//    public static String BASE_FEED = "http://172.16.74.6:8080/pci-user/";
+    public static String BASE_FEED = "http://xlab-tech.com:45675/pci-user/";
 
     public static String HEALTH_PLAN_URL = BASE_FEED + "pci-health-scheme.html?userId=";
 
