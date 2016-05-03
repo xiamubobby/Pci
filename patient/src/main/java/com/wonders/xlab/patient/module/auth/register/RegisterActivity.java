34cbf5258f16b8c45b7130ccf1953af500342509
hua <mzhua78@hotmail.com>
2016-05-03 17:07:47 +0800

    patient:add authorize

--- patient/src/main/java/com/wonders/xlab/patient/module/auth/register/RegisterActivity.java
+++ patient/src/main/java/com/wonders/xlab/patient/module/auth/register/RegisterActivity.java
@@ -1,31 +1,53 @@
 package com.wonders.xlab.patient.module.auth.register;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 import com.wonders.xlab.patient.R;
 
+import java.util.Objects;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import im.hua.library.base.BaseActivity;
+import im.hua.utils.KeyboardUtil;
 
 public class RegisterActivity extends BaseActivity {
+
+    @Bind(R.id.login_phone_number)
+    EditText loginPhoneNumber;
+    @Bind(R.id.et_register_cap)
+    EditText etRegisterCap;
+    @Bind(R.id.btn_register_cap)
+    Button btnRegisterCap;
+    @Bind(R.id.register_password)
+    EditText registerPassword;
+    @Bind(R.id.btn_register)
+    Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_activity);
+        ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
 
+    @OnClick(R.id.container)
+    public void hideKeyboard() {
+        KeyboardUtil.hide(this);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_register,menu);
+        getMenuInflater().inflate(R.menu.menu_register, menu);
         return true;
     }
 
