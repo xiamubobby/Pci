1093e9f4a72f070e6e3b54014b4b4df77efc3c1c
lixuanwu <410230697@qq.com>
2016-05-06 13:54:51 +0800

    我的订单

---
+++ patient/src/main/java/com/wonders/xlab/patient/module/order/OrderListActivity.java
@@ -0,0 +1,20 @@
+package com.wonders.xlab.patient.module.order;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import com.wonders.xlab.patient.R;
+
+import im.hua.library.base.BaseActivity;
+
+/**
+ * Created by jimmy on 16/5/6.
+ */
+public class OrderListActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.order_list_activity);
+    }
+}
