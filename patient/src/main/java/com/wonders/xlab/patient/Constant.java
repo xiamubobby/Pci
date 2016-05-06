04166f271cbb281a2987023b7ae49d504c9f9040
夏木 <natsuki@BobbyShit.local>
2016-05-06 09:58:31 +0800

    1

--- patient/src/main/java/com/wonders/xlab/patient/Constant.java
+++ patient/src/main/java/com/wonders/xlab/patient/Constant.java
@@ -12,15 +12,14 @@ public class Constant {
     public final static String PREF_KEY_IDEAL_BP_RANGE = "pref_key_ideal_bp_range";
 
     public final static String DEFAULT_PORTRAIT = "http://7xp6gb.com2.z0.glb.qiniucdn.com/2.pic.jpg";
-
     /**
      * 刘毅地址
      */
-    public static String BASE_URL = "http://172.16.76.210:8080/";
+//    public static String BASE_URL = "http://172.16.76.210:8080/";
     /**
      * 内网地址
      */
-//    public static String BASE_URL = "http://172.16.74.6:8080/pci-user/";
+    public static String BASE_URL = "http://172.16.74.6:8080/pci-user/";
     /**
      * 外网地址
      */
