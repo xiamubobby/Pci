fd55a4f191126655163a9454e829305375e361e2
夏木 <natsuki@BobbyShit.local>
2016-05-09 21:59:32 +0800

    end@20160509

--- patient/src/main/java/com/wonders/xlab/patient/mvp/api/ServiceAPI.java
+++ patient/src/main/java/com/wonders/xlab/patient/mvp/api/ServiceAPI.java
@@ -1,5 +1,6 @@
 package com.wonders.xlab.patient.mvp.api;
 
+import com.wonders.xlab.patient.mvp.entity.ServiceListEntity;
 import com.wonders.xlab.patient.mvp.entity.ServiceDetailEntity;
 
 import retrofit2.Response;
@@ -8,9 +9,18 @@ import retrofit2.http.Path;
 import rx.Observable;
 
 /**
+ * Created by natsuki on 16/5/6.
  * Created by WZH on 16/5/9.
  */
 public interface ServiceAPI {
+    /**
+     * 获取所有健康服务
+     *
+     * @return
+     */
+    @GET("healthServices/listHealthService")
+    Observable<Response<ServiceListEntity>> listHealthService();
+
     @GET("healthServices/retrieveHealthServiceDetail/{serviceId}")
     Observable<Response<ServiceDetailEntity>> getServiceDetail(@Path("serviceId") Long serviceId);
 }
