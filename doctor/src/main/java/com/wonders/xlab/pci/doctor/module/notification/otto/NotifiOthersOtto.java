5ff9348ea4ccd472d8553ce71d338f475168e0ef
hua <mzhua78@hotmail.com>
2016-05-19 14:08:18 +0800

    remove doctor

--- doctor/src/main/java/com/wonders/xlab/pci/doctor/module/notification/otto/NotifiOthersOtto.java
+++
@@ -1,7 +0,0 @@
-package com.wonders.xlab.pci.doctor.module.notification.otto;
-
-/**
- * Created by hua on 16/4/18.
- */
-public class NotifiOthersOtto {
-}
--- doctor/src/main/java/com/wonders/xlab/pci/doctor/otto/ForceExitOtto.java
+++
@@ -1,7 +0,0 @@
-package com.wonders.xlab.pci.doctor.otto;
-
-/**
- * Created by hua on 16/2/25.
- */
-public class ForceExitOtto {
-}
