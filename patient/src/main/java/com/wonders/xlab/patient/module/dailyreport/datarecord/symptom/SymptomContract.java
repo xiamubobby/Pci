0ae5a1e0f69419dd5ace47b1beec908af6d8eb7a
王志辉 <wangzhihui@wondersgroup.com>
2016-07-05 10:59:18 +0800

    module change

--- patient/src/main/java/com/wonders/xlab/patient/module/dailyreport/datarecord/symptom/SymptomContract.java
+++ patient/src/main/java/com/wonders/xlab/patient/module/dailyreport/datarecord/symptom/SymptomContract.java
@@ -1,8 +1,47 @@
 package com.wonders.xlab.patient.module.dailyreport.datarecord.symptom;
 
+import com.wonders.xlab.patient.mvp.entity.SymptomEntity;
+
+import im.hua.library.base.mvp.IBaseModel;
+import im.hua.library.base.mvp.IBasePresenter;
+import im.hua.library.base.mvp.listener.BaseModelListener;
+import im.hua.library.base.mvp.listener.BasePresenterListener;
+
 /**
  * Created by WZH on 16/6/30.
  */
 public interface SymptomContract {
 
+    interface Callback extends BaseModelListener {
+        void onSaveSymptomSuccess(String message, Callback callback);
+
+        void onReceiveSymptomsSuccess(SymptomEntity.RetValuesEntity valuesEntity, Callback callback);
+    }
+
+    interface Model extends IBaseModel {
+        void getSymptoms();
+
+        void saveSymptom(String userId, String[] symptomIdsStr);
+    }
+
+    interface ViewListener extends BasePresenterListener {
+        void showSymptoms(SymptomEntity.RetValuesEntity valuesEntity);
+
+        void onSaveSymptomSuccess(String message);
+    }
+
+    interface Presenter extends IBasePresenter {
+        /**
+         * 获取全部不适症状
+         */
+        void getSymptoms();
+
+        /**
+         * 保存不适症状
+         *
+         * @param patientId
+         * @param symptomIdsStr
+         */
+        void saveSymptom(String patientId, String[] symptomIdsStr);
+    }
 }
